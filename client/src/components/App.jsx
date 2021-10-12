7a0032b22044f0c3d4378514e36ba945d0f2fe46
Yingchen <pepperbai1114@gmail.com>
2021-10-12 12:31:09 -0700

    set up

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App;
